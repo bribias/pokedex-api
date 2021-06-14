d5893bae6d3ff6dbb8c6d0aff2393206233c0c21
Brianna Bias <brianna.bias@gmail.com>
2021-06-14 16:23:15 -0700

    router and link added

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
     render() {
         return (
             <div className='header-div'>
-                AlcheDex
+                <Link to="/">
+                    Home
+                </Link>
+                <Link to="/pokemon">
+                    Pokemon
+                </Link>
             </div>
         )
     }
